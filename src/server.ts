7c4f721397689e00b0cbd25833cb0f46729f282f
Leonardo Ruda <leonardorudaog@gmail.com>
2023-02-23 15:14:03 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,11 @@ import routes from './routes/index';
 require('dotenv').config();
 
 const server = express();
-
 let viewsPath = path.join(__dirname, 'views');
+
+server.engine('mustache', mustache());
 server.set('view engine', 'mustache');
 server.set('views', viewsPath);
-//server.set('views', path.join(__dirname, 'views'));
-server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
 
@@ -22,5 +21,4 @@ server.use((req, res) => {
 
 server.listen(process.env.PORT || 3333, () => {
     console.log('HTTP server running');
-    console.log(`Views path: `, viewsPath);
   })
